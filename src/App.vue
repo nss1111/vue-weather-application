99e320ab28ae8a66b54c67e41e552583630e81c0
nss1111@ge.corp <nss1111@ge.corp>
2022-03-26 16:46:41 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <HeaderDisplay />
+  <WeatherContent />
+</template>
+
+<script>
+import HeaderDisplay from "./components/HeaderDisplay";
+import WeatherContent from "./components/weather/WeatherContent.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderDisplay,
+    WeatherContent,
+  },
+};
+</script>
+
+<style></style>
